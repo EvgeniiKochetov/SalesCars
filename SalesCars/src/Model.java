53bf9e72894d24b533cd98a8ab4783b393d61fe4
EvgeniiKochetov <KochetovEvgeny@gmail.com>
2020-04-25 21:51:31 +0300

    Add new class Model

---
+++ SalesCars/src/Model.java
@@ -0,0 +1,20 @@
+
+public class Model {
+	
+	String name;
+	int code;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
+}
