299626ec7da2d7341079f8781a040eb39304ba80
Evgeny <Evgeny@notebook>
2020-04-25 19:36:48 +0300

    Add new class Car

---
+++ SalesCars/src/Car.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+
+public class Car {
+	private Color color;
+	private String vin;
+	private double price;
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
